48d58c33dd622adc0a5bc4f4210778bc9aecb6e4
Vikcarvalhos <viktorcarvalhos@gmail.com>
2024-04-09 15:20:12 -0300

    feat: react-icons added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,21 @@
 import {Link} from 'react-router-dom'
-
+import { motion } from "framer-motion"
+g
 function Nav(){
 
     return(
         <>
             <header className='menu'>
                 <nav className='nav-menu'>
-                    <ul>
+                    <div id='nav-logo'>
+                        <img src=''></img>
+                    </div>
+                    <div id='nav-links'>
                         <Link to="/">Home</Link>
                         <Link to="/sobre">Sobre</Link>
                         <Link to="/portifolio">Portifolio</Link>
-                        <Link to="/contato">Contato</Link>
-                    </ul>
+                        <Link to="/contato">Contato</Link> 
+                    </div>               
                 </nav>
             </header>
         </>
