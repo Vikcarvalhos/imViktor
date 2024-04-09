5af57a46f66c08133d0aeaa78afae8edb20c3edc
João Viktor Carvalho de Souza <jcsouza@matec.com.br>
2024-04-09 17:22:39 -0300

    feat: underline hover animation created

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,10 +9,10 @@ function Nav(){
             <header className='menu'>
                 <nav className='nav-menu'>
                     <div id='nav-links'>
-                        <Link to="/">Home</Link>
-                        <Link to="/sobre">Sobre</Link>
-                        <Link to="/portifolio">Portifolio</Link>
-                        <Link to="/contato">Contato</Link> 
+                        <Link to="/" className='hover-underline'>Home</Link>
+                        <Link to="/sobre" className='hover-underline'>Sobre</Link>
+                        <Link to="/portifolio" className='hover-underline'>Portifolio</Link>
+                        <Link to="/contato" className='hover-underline'>Contato</Link> 
                     </div>               
                 </nav>
             </header>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,15 @@
 import '../css/style.css'
+import { motion } from "framer-motion"
 
 function Home(){
 
     return(
         <>
             <main id='main-content'>
-                <div className='home'>
+                <motion.div className='home'>
                     <h1>Home</h1>
                     <p>Seja bem-vindo(a) ao meu site! Aqui você encontrará informações sobre mim e sobre o meu trabalho.</p>
-                </div>
+                </motion.div>
             </main>
         </>
     )
