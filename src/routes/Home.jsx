1d34ab2f5709293c4b0be655027cd96c5a8cd030
João Viktor Carvalho de Souza <jcsouza@matec.com.br>
2024-04-09 11:47:46 -0300

    feat: react added to project

---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import '../css/style.css'
+
+function Home(){
+
+    return(
+        <>
+            <div className='produtos'>
+                Oi
+            </div>
+        </>
+    )
+}
+
+export default Home
